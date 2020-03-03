cecf2617e98cc57740f95de7a9e5cd9877c2c691
unknown <edward.porras@labinfo.is.escuelaing.edu.co>
2020-03-03 13:09:53 -0500

    askd

--- chekers/solve.java
+++ chekers/solve.java
@@ -13,29 +13,34 @@ public class solve{
     private HashMap<String ,Integer[]> posiciones;
     public solve (){
         posiciones = new HashMap<String ,Integer[]>();
-        nose();
         input = new String[8][8];
         output = new String[8][8];
+        nose();
     }
   
     private void nose(){
         int contador = 1;
         for (int i = 0 ; i < 8 ; i++){
             for (int j = 1-i%2 ; j < 8 ; j+=2){
+                input[i][j] = ".";
+                output[i][j] = ".";
                 String pasar = String.valueOf(contador);
                 posiciones.put(pasar , new Integer[] {i,j});
                 contador+=1;
+            }for (int j =i%2 ; j < 8 ; j+=2){
+                input[i][j] = "-";
+                output[i][j] = "-";
             }
         }
     }
     
-    public void move(String turno, int veces){
+    private void move(String turno, int veces){
         if (veces > 0){
-            String ficha = "";
+            String ficha ="b";
             if (turno == "b"){
                 ficha = "w";
             }else{
-                ficha = "b";
+                ficha="b";
             }
             Scanner scan = new Scanner(System.in);
             String turno2 = scan.nextLine();
@@ -43,45 +48,61 @@ public class solve{
             move = new ArrayList<Integer>();
             String[] jumps = turno2.split("x");
             String[] shift = turno2.split("-");
-            //inicio de convertir String a int
             char letra = turno2.charAt(1);
             char letra2 = turno2.charAt(2);
             if (letra == '-' || letra2 == '-'){
                 Integer[] posicion = posiciones.get(shift[0]);
-                input[posicion[0]][posicion[1]] = turno;
+                output[posicion[0]][posicion[1]] = ".";
+                if(input[posicion[0]][posicion[1]]=="."){
+                    input[posicion[0]][posicion[1]] = turno;
+                }
                 posicion = posiciones.get(shift[1]);
                 output[posicion[0]][posicion[1]] = turno;
             }else{
-                Integer[] posicion = posiciones.get(shift[0]);
-                input[posicion[0]][posicion[1]] = turno;
+                Integer[] posicion = posiciones.get(jumps[0]);
+                output[posicion[0]][posicion[1]] = ".";
+                if(input[posicion[0]][posicion[1]]=="."){
+                    input[posicion[0]][posicion[1]] = turno;
+                }
                 for(int i = 1; i< jumps.length; i++){
                     int k1 = 1, k2 =1;
-                    Integer[] posicion2 = posiciones.get(shift[i]);
-                    if (posicion[0] < posicion2[0]){
+                    Integer[] posicion2 = posiciones.get(jumps[i]);
+                    if (posicion[0] > posicion2[0]){
                         k1 = -1;
-                    }if (posicion[1] < posicion2[1]){
+                    }if (posicion[1] > posicion2[1]){
                         k2 = -1;
                     } if (output[posicion[0]+k1][posicion[1]+k2] == "."){
+                        
                         input[posicion[0]+k1][posicion[1]+k2] = ficha;
+                        
                     }else{
-                        input[posicion[0]+k1][posicion[1]+k2] = ".";
+                        output[posicion[0]+k1][posicion[1]+k2] = ".";
                     }
-                    posicion = posiciones.get(shift[i]);
+                    posicion = posiciones.get(jumps[i]);
                 }
                 output[posicion[0]][posicion[1]] = turno;
             }
-            move(ficha, veces-1);
+           move(ficha, veces-1);
         }
     }
-  
-    public void main(String[] args){
-        solve s = new solve();
-        Scanner scan = new Scanner(System.in);
-        String turno = scan.nextLine();
-        String[] veces = turno.split(" ");
-        int entero = Integer.parseInt(veces[1]);    
-        move(veces[0],entero);
+    public void solve(String s, int t){
+        move(s,t);
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                System.out.print(input[i][j]);
+            }
+            System.out.print(" ");
+            for(int j=0;j<8;j++){
+                System.out.print(output[i][j]);
+            }
+            System.out.println("");
+        }
     }
+    public void simulate(String s,int t){
+        
+    }
+    
+
         
  }
 
